3a3d0ed6803ced0436b01966e1d5d6416ed911e7
Efrexz <zyzz_448@hotmail.com>
2025-10-16 16:45:28 -0500

    agregando seccion de carousel de productos

---
+++ src/data/productsData.js
@@ -0,0 +1,67 @@
+const featuredProducts = [
+  {
+    id: 1,
+    name: "Dog Kibble (Large)",
+    description: "Hypoallergenic Salmon & Rice Formula.",
+    price: "$45.99",
+    image:
+      "https://images.unsplash.com/photo-1596272875729-ed2ff7d6d9c5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170",
+    color: "amber-700",
+  },
+  {
+    id: 2,
+    name: "Feline Vitamins (30 tabs)",
+    description: "Essential multivitamins for coat health.",
+    price: "$18.50",
+    image:
+      "https://images.unsplash.com/photo-1596272875729-ed2ff7d6d9c5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170",
+    color: "green-700",
+  },
+  {
+    id: 3,
+    name: "Flea & Tick Treatment",
+    description: "Effective broad-spectrum protection.",
+    price: "$32.00",
+    image:
+      "https://images.unsplash.com/photo-1596272875729-ed2ff7d6d9c5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170",
+    color: "orange-700",
+  },
+  {
+    id: 4,
+    name: "Grooming Kit",
+    description: "Everything you need for a shiny coat.",
+    price: "$55.00",
+    image:
+      "https://images.unsplash.com/photo-1596272875729-ed2ff7d6d9c5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170",
+    color: "gray-800",
+  },
+  {
+    id: 5,
+    name: "Clumping Cat Litter",
+    description: "Odor control and superior absorption.",
+    price: "$22.99",
+    image:
+      "https://images.unsplash.com/photo-1596272875729-ed2ff7d6d9c5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170",
+    color: "amber-700",
+  },
+  {
+    id: 6,
+    name: "Feline Vitamins (30 tabs)",
+    description: "Essential multivitamins for coat health.",
+    price: "$18.50",
+    image:
+      "https://images.unsplash.com/photo-1596272875729-ed2ff7d6d9c5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170",
+    color: "green-700",
+  },
+  {
+    id: 7,
+    name: "Feline Vitamins (30 tabs)",
+    description: "Essential multivitamins for coat health.",
+    price: "$18.50",
+    image:
+      "https://images.unsplash.com/photo-1596272875729-ed2ff7d6d9c5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170",
+    color: "green-700",
+  },
+];
+
+export { featuredProducts };
